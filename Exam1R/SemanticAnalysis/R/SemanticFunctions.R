b81edf00f3cf8b61935bb712c28b5c5c936d54d5
Leo <leo.kowalski@ki.se>
2024-11-22 07:57:12 +0100

    Third commit

--- Exam1R/SemanticAnalysis/R/SemanticFunctions.R
+++ Exam1R/SemanticAnalysis/R/SemanticFunctions.R
@@ -4,3 +4,25 @@ use_git()
 ls(package:usethis)
 use_mit_license()
 use_github()
+
+
+
+##### Prepare the text file
+
+TextPrep <- function(file) {
+  Text <- readLines(file)
+  lowercase_text <- tolower(Text)
+ # return(Text)
+  strsplit()
+  return(lowercase_text)
+}
+
+### Read into environment
+TextPrep("C:/Users/leokow/Downloads/Example_negative.txt")
+
+Text <- TextPrep("C:/Users/leokow/Downloads/Example_negative.txt")
+
+print(Text)
+tolower(Text)
+
+
